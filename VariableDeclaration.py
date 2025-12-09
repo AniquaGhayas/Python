b0f3f385942f70718e22c35ad4b091062980cb43
AniquaGhayas <aniquaghayas@gmail.com>
2025-12-09 18:04:09 +0530

    VariableDeclaration.py

---
+++ VariableDeclaration.py
@@ -0,0 +1,18 @@
+a = 10              # Integer
+b = 3.14            # Float
+c = "Hello Python"  # String
+d = True            # Boolean
+e = [1, 2, 3, 4]    # List
+f = (5, 6, 7)       # Tuple
+g = {1, 2, 3}       # Set
+h = {"name": "Anny", "age": 20}  # Dictionary
+
+print("Value of a:", a, " | Data Type:", type(a))
+print("Value of b:", b, " | Data Type:", type(b))
+print("Value of c:", c, " | Data Type:", type(c))
+print("Value of d:", d, " | Data Type:", type(d))
+print("Value of e:", e, " | Data Type:", type(e))
+print("Value of f:", f, " | Data Type:", type(f))
+print("Value of g:", g, " | Data Type:", type(g))
+print("Value of h:", h, " | Data Type:", type(h))
+
