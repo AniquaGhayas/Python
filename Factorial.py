33efc95b63398e519a44a73526efb899227cb32b
AniquaGhayas <aniquaghayas@gmail.com>
2025-12-09 23:36:56 +0530

    Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,7 @@
 def fact(n):
     if n == 0:
         return 1
-    return n * fact(n-1)
+    else:
+        return n * fact(n - 1)
 num = int(input("Enter a number: "))
 print("Factorial of", num, "is", fact(num))
