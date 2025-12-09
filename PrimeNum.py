65f1e5463b8dac0628c499bb400d329747bfc475
AniquaGhayas <aniquaghayas@gmail.com>
2025-12-09 18:12:08 +0530

    PrimeNum.py

--- PrimeNum.py
+++ PrimeNum.py
@@ -1,6 +1,6 @@
 num = int(input("Enter a number: "))
 if num > 1:
- for i in range(2, num):
+ for i in range(2, int((num**0.5)+1)):
     if num % i == 0:
        print(num, "is not a prime number")
        break
@@ -8,3 +8,4 @@ if num > 1:
     print(num, "is a prime number")
 else:
  print(num, "is not a prime number")
+
