3892d839ae8e1ea4a07396fba091cb64d8d6b2e8
AniquaGhayas <aniquaghayas@gmail.com>
2025-12-10 17:04:47 +0530

    Pandas_dataframe_creation.py

---
+++ Pandas_dataframe_creation.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data = {
+ "Name": ["Ali", "Umar", "Usman"],
+ "Age": [18, 19, 18],
+ "Course": ["CSE", "ECE", "MECH"] }
+df = pd.DataFrame(data)
+print("Data Frame:")
+print(df)
