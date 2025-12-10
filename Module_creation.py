3b3af055661fbad3de1857a88bdb3f7fd1bf7d84
AniquaGhayas <aniquaghayas@gmail.com>
2025-12-10 16:36:31 +0530

    Module_creation.py

---
+++ Module_creation.py
@@ -0,0 +1,13 @@
+#both of these files should remain in the same folder for the module to be imported and used  
+#create a module by saving this part of the code as mymodule.py file 
+
+#mymodule.py
+def greet(name):
+ return "Hello, " + name
+
+#import the mymodule.py file in the main.py file 
+
+#main.py
+from mymodule import greet
+name = input("Enter your name: ")
+print(greet(name))
