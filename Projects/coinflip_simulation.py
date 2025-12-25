977164ee6874ffc9e940a6b5a53687655961b724
AniquaGhayas <aniquaghayas@gmail.com>
2025-12-25 22:26:02 +0530

    coinflip_simulation.py

---
+++ Projects/coinflip_simulation.py
@@ -0,0 +1,10 @@
+##this program simulates a coin toss: ≈ 50% of the time, it's "Heads", ≈ 50% of the time, it's "Tails"
+
+import random
+
+num = random.randint(0, 1)   # Generates a random number that's either 0 or 1
+
+if num > 0.5:
+  print('Heads')
+else:
+  print('Tails')
