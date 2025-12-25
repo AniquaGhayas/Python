13cf96ad47f5dc957e9d3b614a62b46deef0e07c
AniquaGhayas <aniquaghayas@gmail.com>
2025-12-25 22:32:03 +0530

    magic8.py

---
+++ Projects/magic8.py
@@ -0,0 +1,40 @@
+# The Magic 8 Ball is a popular office toy and children's toy invented in the 1940s for fortune-telling and advice seeking. 🎱
+
+# It's an oversized 8 ball with some of the following answers:
+
+#Yes - definitely.
+#It is decidedly so.
+#Without a doubt.
+#Reply hazy, try again.
+#Ask again later.
+#Better not tell you now.
+#My sources say no.
+#Outlook not so good.
+#Very doubtful.
+
+#A program that can respond to any Yes or No questions with a different answer each time it executes. 
+
+import random
+
+question = input('Question: ')
+
+random_number = random.randint(1, 9)
+
+if random_number == 1:
+  print("Magic 8 ball: Yes - definitely")
+elif random_number == 2:
+  print("Magic 8 ball: It is decidedly so")
+elif random_number == 3:
+  print("Magic 8 ball: Without a doubt")
+elif random_number == 4:
+  print("Magic 8 ball: Reply hazy, try again")
+elif random_number == 5:
+  print("Magic 8 ball: Ask again later")
+elif random_number == 6:
+  print("Magic 8 ball: Better not tell you now")
+elif random_number == 7:
+  print("Magic 8 ball: My sources say no")
+elif random_number == 8:
+  print("Magic 8 ball: Outlook not so good")
+else:
+  print("Magic 8 ball: Very doubtful")
